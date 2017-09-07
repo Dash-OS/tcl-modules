e990b7f6bec909ac9b55efbe02d8b0f89afe6450
bradennapier <brady.napier@dashos.net>
2017-09-07 14:45:45 -0700

    [2.090717.76] 09/07/17 at 02:45 PM | by DashBot

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,7 +1,6 @@
 package ifneeded tcl-modules 1.0 [list apply {{dir} {
   # simply add our tcl-modules path to the tm path
   # so they can be required.
-  puts [info script]
-  puts "dir $dir"
   ::tcl::tm::path add $dir
+  package provide tcl-modules 1.0
 }} $dir]
